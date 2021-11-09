33f30218263f0d44532da96ab1691dd4327c8908
RuslanFilin <r.golpo322@gmail.com>
2021-11-09 16:27:33 +0300

    part3

---
+++ js/main3.js
@@ -0,0 +1,72 @@
+let students = [
+    {
+        id: '',
+        name: "Григорян Артур Тигранович",
+        group: "РПЗ 18 1/9",
+        year: "2002",
+        pr1: false,
+    },
+    {
+        id: '',
+        name: "Кабанов Данил Алексеевич",
+        group: "РПЗ 18 1/9",
+        year: "2003",
+        pr1: false,
+    },
+    {
+        id: '',
+        name: "Ключка Никита Сергеевич",
+        group: "РПЗ 18 1/9",
+        year: "2002",
+        pr1: false,
+    },
+    {
+        id: '',
+        name: "Коршук Руслан Сергеевич",
+        group: "РПЗ 18 1/9",
+        year: "2003",
+        pr1: true,
+    },
+    {
+        id: '',
+        name: "Нугманов Руслан Олегович",
+        group: "РПЗ 18 1/9",
+        year: "2001",
+        pr1: true,
+    },
+    {
+        id: '',
+        name: "Паюнин Андрей Витальевич",
+        group: "РПЗ 18 1/9",
+        year: "2003",
+        pr1: true,
+    },
+    {
+        id: '',
+        name: "Савченко Роман Юрьевич",
+        group: "РПЗ 18 1/9",
+        year: "2003",
+        pr1: false,
+    },
+    {
+        id: '',
+        name: "Толстов Олег Витальевич",
+        group: "РПЗ 18 1/9",
+        year: "2002",
+        pr1: true,
+    },
+]
+var thirdDay = new Vue ({
+    el: '#thirdDay',
+    data: {
+        stud: students.slice(),
+        filter_name: '',
+    },
+    methods: {
+        deleteStud: function(row) {
+            for (let i = 0; i < this.stud.length; i++)
+                if (Number(i) === Number(row))
+                    this.stud.splice(i,1);
+        }
+    }
+})
